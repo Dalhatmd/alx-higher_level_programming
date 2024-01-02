71042f5d2f65b09eec28666d164d130fa1d08606
Dalhatmd <umarwalid4055@gmail.com>
2024-01-02 19:35:46 +0100

    Fix again

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def remove_char_at(word, n):
     copy = str(word)
+    if (n < 0):
+        return word
     try:
         copy = copy.replace(copy[n], '')
-    except:
+    except IndexError:
         return word
     return copy
-
-#print(remove_char_at("Chicago", 16))
