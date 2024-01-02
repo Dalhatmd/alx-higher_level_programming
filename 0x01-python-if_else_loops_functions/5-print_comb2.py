ee4a929ea8fa8ccb9e1d33f4884c463b6ace3c84
Dalhatmd <umarwalid4055@gmail.com>
2024-01-02 22:04:36 +0100

    Fix 5-print_comb.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""for i in range(10):
-    for ii in range(10):
-        if ii == 9 and i == 9:
-            print("{a}{b}".format(a=i, b=ii))
-            break
-        print("{a}{b}".format(a=i, b=ii), end=', ')"""
-
-
+for i in range(99):
+    print("{:02d}".format(i), end=' ')
+print(99)
