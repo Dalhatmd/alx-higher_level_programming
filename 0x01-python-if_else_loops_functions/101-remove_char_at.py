33aef92691d75284a1c8477edf6228892fd61f6b
Dalhatmd <umarwalid4055@gmail.com>
2024-01-02 19:30:57 +0100

    fix 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def remove_char_at(word, n):
     copy = str(word)
-    copy = copy.replace(copy[n], '')
+    try:
+        copy = copy.replace(copy[n], '')
+    except:
+        return word
     return copy
+
+#print(remove_char_at("Chicago", 16))
