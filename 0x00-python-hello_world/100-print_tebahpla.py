09c2ce2c00bf55d29e07e654f9a92ce74059c938
Dalhatmd <umarwalid4055@gmail.com>
2024-01-03 23:01:14 +0100

    Finished 8-concat.py

--- 0x00-python-hello_world/100-print_tebahpla.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -2):
-    if 'a' <= chr(i) <= 'z':
-        print("{}".format(chr(i)), end='')
-    if 'A' <= chr(i-1) <= 'Z':
-        print("{}".format(chr(i - 1)), end='')
