ad6a0d179a79e6c97220816cb50362acda12cea7
Dalhatmd <umarwalid4055@gmail.com>
2024-01-03 19:25:39 +0100

    Added 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ *
+ * @list: list to be checked
+ *
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *var;
+	listint_t *check = NULL;
+
+	var = list;
+	while (var != NULL)
+	{
+		var = var->next;
+		if (var == check)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
