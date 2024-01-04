7018bbe99615bdcd46a0eed1aa93802752ea4a32
Dalhatmd <umarwalid4055@gmail.com>
2024-01-04 11:18:55 +0100

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+num = len(sys.argv)
+args = sys.argv
+i = 1
+if num == 1:
+    print("{} arguments.".format(num - 1))
+if num > 1:
+    print("{} arguments:".format(num - 1))
+    for arg in args[1:]:
+        print("{}: {}".format(i, arg))
+        i += 1
