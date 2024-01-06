f745a5d6d06bca8872d6e8ace515cfa15980f98f
Dalhatmd <umarwalid4055@gmail.com>
2024-01-06 12:27:25 +0100

    Fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return (len(sentence), None)
     length, first = (len(sentence), sentence[0])
 
     return (length, first)
