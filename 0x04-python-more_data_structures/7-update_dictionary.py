d7c1c92f40fc6b3089941be4050382d366efae86
Dalhatmd <umarwalid4055@gmail.com>
2024-01-10 11:44:21 +0100

    add 7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    try:
+        a_dictionary[key] = value
+        print(a_dictionary)
+        return a_dictionary
+    except KeyError:
+        return None
