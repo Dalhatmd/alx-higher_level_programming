07b56fa4a484ad99842e5c6e348fa163eded00c2
Dalhatmd <umarwalid4055@gmail.com>
2024-01-23 13:04:36 +0100

    fix documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+# 1-square.py by Dalhatmd
+""" Defines a square class """
+
+
 class Square:
-    """ A class that instantiates a square with a private instamce size
-    """
+    """ A class that instantiates a square with a private instamce size"""
+
     def __init__(self, size):
-        """ Instantiation with size 
-        Args:
-            size(int): size of the square
+        """ Instantiation with size
+        Args: size - size of the square
         """
+
         self.__size = size
