adccb8d7bffa1bf07cb585e4fad519c0e0916b84
Dalhatmd <umarwalid4055@gmail.com>
2024-01-23 17:17:45 +0100

    fix pycodestyle and add documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+# 2-square.py by Dalhatmd
+"""A square definition"""
+
+
 class Square:
     """ A class that defines a square
     """
     def __init__(self, size=0):
         """Initializer for the square class
-        Args:
-            size(int): size of the square
+        Argss: size - size of the square
         """
         self.__size = size
         if type(size) is not int:
