e74841a38e2bee17f17b0fca2d916530af80d913
Dalhatmd <umarwalid4055@gmail.com>
2024-01-25 14:34:39 +0100

    Add task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    if not (isinstance(a, int) or not isinstance(b, int)):
-        try:
-            a = int(a)
-            b = int(b)
-        except ValueError:
-            raise ValueError("Both inputs must be integers")
-    return (a + b)
+""" program that adds 2 integers """
 
-import doctest
-doctest.testfile("0-add_integer.txt")
+
+def add_integer(a, b=98):
+    """ adds 2 integers
+    Args:
+        a - first integer
+        b - second integer
+    Raises: Value Error if argument is not an integer
+    Returns: a + b
+    """
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise ValueError("a must be an integer")
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise ValueError("b must be an integer")
+
+    return int(a) + int(b)
