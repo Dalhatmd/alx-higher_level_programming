e87dc4174e042e54dbc7cd3e600f66bbbeb9bef0
Dalhatmd <umarwalid4055@gmail.com>
2024-01-26 15:26:10 +0100

    Add singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -4,35 +4,71 @@
 
 
 class Node:
-    def __init__(self, data, next_node= None):
-        if not isinstance(data, int):
-            raise TypeError("data must be an integer")
-        self.__data = data
-        if not isinstance(next_node, Node) and next_node is not None:
-            raise TypeError("next_node must be a Node object")
+    """ Node class definition """
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
     @property
     def next_node(self):
-        return (self.__next_node)
+        """ property getter for next_nodr """
+
+        return (self._next_node)
 
     @next_node.setter
     def next_node(self, value):
+        """ next_mode setter """
+
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+        self._next_node = value
 
     @property
     def data(self):
-        return (self.__data)
+        """ data getter """
+
+        return (self._data)
 
     @data.setter
-    def data(self_value):
+    def data(self, value):
+        """ data setter """
+
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
-        self.__data = value
+        self._data = value
+
 
 class SinglyLinkedList:
+    """ Singly linked list class """
     def __init__(self):
-        pass
-    def sorted_insert(self, value):
+        """initialiser for the class """
 
+        self.head = None
+
+    def sorted_insert(self, value):
+        """ inserts a node in a sorted manned """
+
+        new_node = Node(value)
+
+        if not self.head or value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next_node and current.next_node.data < value:
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
+
+    def __str__(self):
+        """ Makes linked list printable"""
+
+        result = []
+        current = self.head
+        while current:
+            result.append(current.data)
+            current = current.next_node
+        return '\n'.join(map(str, result))
