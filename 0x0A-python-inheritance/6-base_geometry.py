3c8fae70d39789acdbcb638ce42c0824eae833f3
Dalhatmd <umarwalid4055@gmail.com>
2024-02-05 15:53:05 +0100

    fix pycodestyle

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -3,10 +3,11 @@
 
 
 class BaseGeometry:
-        """ A badegeometry class"""
-        def __init__(self):
-            pass
+    """ A badegeometry class"""
+    def __init__(self):
+        """ initializer """
+        pass
 
-        def area(self):
-            """ Raises an exception """
-            raise Exception("area is not implemented")
+    def area(self):
+        """ Raises an exception """
+        raise Exception("area is not implemented")
