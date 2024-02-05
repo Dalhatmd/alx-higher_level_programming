903db62e8c260bf7427afd80beaca82b88e97847
Dalhatmd <umarwalid4055@gmail.com>
2024-02-05 21:07:28 +0100

    Add task 100x

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A module defining my integer class """
+
+class MyInt(int):
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return not super().__eq__(other)
+
+    def __ne__(self, other):
+        if isinstance(other, int):
+            return not super().__ne__(other)
