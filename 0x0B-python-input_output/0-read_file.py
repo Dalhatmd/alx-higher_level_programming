ebeb63cdc9b80223f08a888ac460e5a1e627ae02
Dalhatmd <umarwalid4055@gmail.com>
2024-02-06 06:32:50 +0100

    Add task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module to read a file """
+
+
+def read_file(filename=""):
+    """ Reads a file """
+    with open(filename, "r") as file:
+        data = file.read()
+        print(data)
