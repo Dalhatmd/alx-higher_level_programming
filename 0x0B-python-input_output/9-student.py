b2a60c6cc378856e8c58d7a39dcfe1b28cd70010
Dalhatmd <umarwalid4055@gmail.com>
2024-02-06 20:31:55 +0100

    Add task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,12 +2,16 @@
 """A module that defines a student class """
 
 
+class_to_json = __import__('8-class_to_json').class_to_json
+
+
 class Student:
-    """ A studemt blueprint """
+    """ A student blueprint """
     def __init__(self, first_name, last_name, age):
+        """initialiser for student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        pass
+        return class_to_json(self)
