f1ad3d3c467ab6d98b3dc1361765779e0e7e9954
Dalhatmd <umarwalid4055@gmail.com>
2024-02-07 16:44:59 +0100

    Add task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A module that computes Pascal's triangle"""
+
+
+def pascal_triangle(n):
+    triangle = []
+    for row in range(n):
+        current_row = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                current_row.append(1)
+            else:
+                previous_row = triangle[row - 1]
+                current_row.append(previous_row[col - 1] + previous_row[col])
+        triangle.append(current_row)
+    return triangle
