bc02c8ea88a11d27552a2d0312412e6391ebbf5f
Dalhatmd <umarwalid4055@gmail.com>
2024-02-07 15:31:33 +0100

    Add task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""A student class module """
+
+
 class Student:
     """ A student blueprint """
     def __init__(self, first_name, last_name, age):
@@ -6,8 +10,15 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-        def to_json(self, attrs = None):
-            """Returns a json representation of the object"""
-            if attrs is type(list):
-
-            return class_to_json(self)
+    def to_json(self, attrs=None):
+        """Returns a json representation of the object"""
+        if attrs is None:
+            return {
+                    "first_name": self.first_name,
+                    "last_name": self.last_name,
+                    "age": self.age
+                    }
+        else:
+            return {
+                    attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
