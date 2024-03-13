d6aefad5d80233dd869aeb1ef1588051cda3451e
Dalhatmd <umarwalid4055@gmail.com>
2024-03-13 16:33:19 +0100

    Add task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Displays all records of the table 'second_table'. 
+-- Displays both the score and the name
+-- Order by score (top first)
+
+SELECT score, name FROM second_table
+ORDER BY score DESC;
