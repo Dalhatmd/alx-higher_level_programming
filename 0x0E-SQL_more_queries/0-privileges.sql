6d94ab3c149f9568278c8bffc61ef5a496107aba
Dalhatmd <umarwalid4055@gmail.com>
2024-03-14 15:18:12 +0100

    Fix typo

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Displays grants for user_0d_1 and user_0d_2
 
-SHOW GRANTS FOR user_od_1@localhost;
-SHOW GRANTS FOR user_od_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
