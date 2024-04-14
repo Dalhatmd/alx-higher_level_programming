3cb107e215088640404bd425163ab277908ee636
Dalhatmd <umarwalid4055@gmail.com>
2024-04-14 14:49:32 +0100

    Add BINARY condition

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ def main():
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER\
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER\
                 BY states.id ASC")
     rows = cur.fetchall()
 
