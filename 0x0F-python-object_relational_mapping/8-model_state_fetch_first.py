779f05bea112bc7cbecc28e9c5dade5f7888c7d7
Dalhatmd <umarwalid4055@gmail.com>
2024-04-15 22:25:51 +0100

    Add task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Script that quesries a database using the sqlalchemy orm"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+
+def main():
+    """ selects the first name in the states table"""
+    db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3])
+    engine = create_engine(db)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state:
+        print(f"1: {first_state.name}")
+
+
+if __name__ == "__main__":
+    main()
