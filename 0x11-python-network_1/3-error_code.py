198631755f79dd4d84908eb324284936034f6d1c
Dalhatmd <umarwalid4055@gmail.com>
2024-04-26 15:06:37 +0100

    Add task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Handles error in HTTP response """
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    r = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(r) as response:
+        try:
+            print(response.decode('utf-8'))
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
