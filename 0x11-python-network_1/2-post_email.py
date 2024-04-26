84d7338e18715c34f3f7119e8d8e354630b90043
Dalhatmd <umarwalid4055@gmail.com>
2024-04-26 13:01:05 +0100

    Add task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Posts an email to a url """
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    data = {"emal": "umarmd709@gmail.com"}
+    post_data = urllib.parse.urlencode(data)
+    post_data = post_data.encode('ascii')
+
+    r = urllib.request.Request(sys.argv[1], post_data)
+    with urllib.request.urlopen(r) as response:
+        page = response.read()
