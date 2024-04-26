d982d926dc12c546da8155fc86c2e623853041ff
Dalhatmd <umarwalid4055@gmail.com>
2024-04-26 15:22:52 +0100

    Fix try except statement

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,14 +2,13 @@
 """Handles error in HTTP response """
 import sys
 import urllib.request
-import urllib.parse
 import urllib.error
 
 
 if __name__ == "__main__":
     r = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(r) as response:
         try:
-            print(response.read().decode('utf-8'))
+            with urllib.request.urlopen(r) as response:
+                print(response.read().decode('utf-8'))
         except urllib.error.HTTPError as e:
             print("Error code: {}".format(e.code))
