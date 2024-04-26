0e745e70dfe3dec563ab72aa7d7bbfb1f4b7ddf6
Dalhatmd <umarwalid4055@gmail.com>
2024-04-26 15:25:01 +0100

    Fix pycodestyle

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,8 +7,8 @@ import urllib.error
 
 if __name__ == "__main__":
     r = urllib.request.Request(sys.argv[1])
-        try:
-            with urllib.request.urlopen(r) as response:
-                print(response.read().decode('utf-8'))
-        except urllib.error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    try:
+        with urllib.request.urlopen(r) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
