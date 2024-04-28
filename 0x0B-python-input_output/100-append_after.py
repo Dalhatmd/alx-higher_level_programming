667bc504c941a41e03bc65e79f105fbf575d1b33
Dalhatmd <umarwalid4055@gmail.com>
2024-04-28 10:46:37 +0100

    Add task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,8 +4,14 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """ appends a file """
-    with open(filename, 'a+') as file:
-        for i in file:
-            if i == search_string:
-                text = file.write(search_string)
-        return text
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    modified_lines = []
+    for line in lines:
+        modified_lines.append(line)
+        if search_string in line:
+            modified_lines.append(new_string)
+
+    with open(filename, 'w') as file:
+        file.writelines(modified_lines)
