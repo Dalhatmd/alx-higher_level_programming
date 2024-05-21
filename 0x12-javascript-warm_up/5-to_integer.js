ec45c656079eafb5646fb78a76e3ba2dde246d52
Dalhatmd <umarwalid4055@gmail.com>
2024-05-21 14:10:39 +0100

    Add task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num) || process.argv[2] === undefined) {
+  console.log('Not a number');
+} else {
+  console.log('My Number: ' + num);
+}
