c4427cabec899603c716d7b165dc7465693a6a23
Dalhatmd <umarwalid4055@gmail.com>
2024-05-21 14:47:57 +0100

    Add task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+if (process.argv.length < 2 || isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing size');
+}
+
+const size = parseInt(process.argv[2]);
+
+for (let i = 0; i < size; i++) {
+  let row = '';
+  for (let j = 0; j < size; j++) {
+    row += 'x';
+  }
+  console.log(row);
+}
