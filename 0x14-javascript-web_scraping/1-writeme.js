9aa9bb474a26e4be34d4c29604533ebe117011d8
Dalhatmd <umarwalid4055@gmail.com>
2024-05-21 18:03:52 +0100

    Add task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const data = process.argv[3];
+
+const file = process.argv[2];
+
+fs.writeFile(file, data, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
