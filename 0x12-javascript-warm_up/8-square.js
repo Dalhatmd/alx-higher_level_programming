085933fcfcf99af58c295bfbaff374339911f3c5
Dalhatmd <umarwalid4055@gmail.com>
2024-05-21 16:48:58 +0100

    Fix typo

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -9,7 +9,7 @@ const size = parseInt(process.argv[2]);
 for (let i = 0; i < size; i++) {
   let row = '';
   for (let j = 0; j < size; j++) {
-    row += 'x';
+    row += 'X';
   }
   console.log(row);
 }
