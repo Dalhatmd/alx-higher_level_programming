465646518dea50f266eda20fe700f61e0cd8ec81
Dalhatmd <umarwalid4055@gmail.com>
2024-05-21 14:14:08 +0100

    Fix typo

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const num = parseInt(process.argv[2]);
 if (isNaN(num) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My Number: ' + num);
+  console.log('My number: ' + num);
 }
